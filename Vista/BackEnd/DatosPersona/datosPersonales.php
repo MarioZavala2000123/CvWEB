12832684d8dcd4d1a90abdecb9da486c102b890e
MarioZ26 <maritor2609@gmail.com>
2023-03-11 09:15:07 -0600

    falta testeo con Otto, validaciones y not null

--- Vista/BackEnd/DatosPersona/datosPersonales.php
+++ Vista/BackEnd/DatosPersona/datosPersonales.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
        // echo $vCorreo;
     }
 
-    // if (guardarDatosPersona($vNombre, $vApellido, $vDPI, $vFNacimiento, $vECivil, vPresentacion, $vTelefono, $vCorreo)){
+    // if (datosPersonales($vNombre, $vApellido, $vDPI, $vFNacimiento, $vECivil, vPresentacion, $vTelefono, $vCorreo)){
     //     header('Location: datosAcademicos.html');
     // }else {
     //     //mostrar mensaje de error 
