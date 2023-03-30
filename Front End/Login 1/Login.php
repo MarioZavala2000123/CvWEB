e075e6b7109aa0fe8de6e0000ac1675e1cb56f79
Bryan <brsandovalc@alumno.uspg.edu.gt>
2023-03-30 16:16:58 -0600

    Arregle Login

--- Front End/Login 1/Login.php
+++ Front End/Login 1/Login.php
@@ -1,31 +1,13 @@
 <?php
 
 // Tengo que incluir el archivo de otto
-require_once("../Controlador/Funciones/insert.php");
+require_once("../Controlador/Funciones/GetUsuario.php");
 
 // Proceso de datos cunado se envian
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //Comprobar si el usuario esta vacio
-    if(empty(trim($_POST["vNombreLogin"]))); {
-        $vNombreLogin_err = "Porfavor ingrese su Nombre";
-    
-    } else {
-        $vNombreLogin = trim($_POST[$vNombreLogin]);
-        // echo "vNombreLogin";
-        // echo $vNombreLogin;
-    }
-
-    if(empty(trim($_POST["vApellidoLogin"]))); {
-        $vApellidoLogin_err = "Porfavor ingrese su Apellido";
-    
-    } else {
-        $vApellidoLogin = trim($_POST[$vApellidoLogin]);
-        // echo "vApellidoLogin";
-        // echo $vApellidoLogin;
-    }
-
     if(empty(trim($_POST["vCorreoLogin"]))); {
         $vCorreoLogin_err = "Porfavor ingrese su Correo";
     
@@ -45,11 +27,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
 
-    if (Login($vNombreLogin, $vApellidoLogin, vCorreoLogin, $vContraseñaLogin)){
+    if (Login($vCorreoLogin, $vContraseñaLogin)){
         header('Location: Login.html');
     } else {
         //Mostrar mensaje de error
-        header('Location: Login.html')
+        header('Location: Login.php') 
     }
 }
 ?>
